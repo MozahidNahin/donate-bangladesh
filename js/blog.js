431f23595ae6a17788d72ad16a3de3da796ba0bf
MozahidNahin <mozahidulislamnahin@gmail.com>
2024-09-23 11:46:13 +0600

    designed and linked blog.html page with index.html

---
+++ js/blog.js
@@ -0,0 +1,7 @@
+
+document.getElementById('blog-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    console.log('blog button clicked');
+    window.location.href = 'blog.html';
+})
+
